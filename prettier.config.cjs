395a40d36454066992c20c808c837f66a947ca69
pingubello <102923079+pingubello@users.noreply.github.com>
2023-07-19 14:24:56 +0200

    install tailwind

---
+++ prettier.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: ["prettier-plugin-tailwindcss"],
+  bracketSameLine: true,
+  printWidth: 120,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 4,
+};
---
+++ tailwind.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  plugins: [],
+  theme: {
+    extend: {
+      colors: {
+        background: "#ffefd3",
+        "dark-background": "#f3debd",
+        "dark-green": "#183a37",
+        orange: "#c44900",
+        "dark-purple": "#432534",
+        text: "#04151f",
+        wall: "#858b82",
+        pullover: "#794323",
+        jacket: "#cfcbb7",
+        "light-background": "#fff6e8",
+        hair: "#634944",
+      },
+    },
+  },
+};
