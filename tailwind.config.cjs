bd23ef9abf6891ccdf3d355e69d3e6b9afcc5d82
pingubello <102923079+pingubello@users.noreply.github.com>
2023-07-25 20:19:09 +0200

    passwort, impressum und kontaktseite

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,26 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
+const plugin = require("tailwindcss/plugin");
+const forms = require("@tailwindcss/forms");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./**/*.{html,js}"],
-  plugins: [],
+  plugins: [
+    forms({
+      strategy: "base", // only generate global styles
+    }),
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          "translate-z": (value) => ({
+            "--tw-translate-z": value,
+            transform: ` translate3d(var(--tw-translate-x), var(--tw-translate-y), var(--tw-translate-z)) rotate(var(--tw-rotate)) skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y)) scaleX(var(--tw-scale-x)) scaleY(var(--tw-scale-y))`,
+          }), // this is actual CSS
+        },
+        { values: theme("translate"), supportsNegativeValues: true },
+      );
+    }),
+  ],
   theme: {
     extend: {
       screens: {
